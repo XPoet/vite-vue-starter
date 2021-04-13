e3d4bb68cdb0b528b71673ae2a6212e19dc9b7de
XPoet <i@xpoet.cn>
2021-04-13 19:50:42 +0800

    style: lint vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
   server: {
     port: 4500, // 设置服务启动端口号
     open: true, // 设置服务启动时是否自动打开浏览器
-    cors: true, // 允许跨域
+    cors: true // 允许跨域
 
     // 设置代理，根据我们项目实际情况配置
     // proxy: {
     //   '/api': {
-    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     target: 'http://xxx.xxx.xxx.xxx:x000',
     //     changeOrigin: true,
     //     secure: false,
     //     rewrite: (path) => path.replace('/api/', '/')

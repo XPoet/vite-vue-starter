ee2ef535c13a041e6246acd19a28ab660e26a308
XPoet <i@xpoet.cn>
2021-04-14 15:00:25 +0800

    chore(views): rename page components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="home-container page-container">
-    <img
-      class="vue-element-plus-logo"
-      alt="Vue logo"
-      src="../assets/logo.png"
-    />
+    <img class="vue-element-plus-logo" alt="Vue logo" src="../assets/logo.png" />
     <div class="page-title">Vite2.x + Vue3.x + TypeScript + Element Plus</div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({

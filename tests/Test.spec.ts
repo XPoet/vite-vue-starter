1b4c444a0abe7ca4cc50dd8ef5c5fc98cec8281b
XPoet <i@xpoet.cn>
2021-04-14 19:19:01 +0800

    test: update Test.spec.ts

--- tests/Test.spec.ts
+++ tests/Test.spec.ts
@@ -5,6 +5,6 @@ test('Test.vue', async () => {
   const wrapper = mount(Test)
   expect(wrapper.html()).toContain('Unit Test Page')
   expect(wrapper.html()).toContain('count is: 0')
-  await wrapper.find('el-button').trigger('click')
+  await wrapper.find('button').trigger('click')
   expect(wrapper.html()).toContain('count is: 1')
 })

e66707e5125fac86729f1f442806ac790550b733
XPoet <i@xpoet.cn>
2021-04-14 15:27:39 +0800

    feat(views): add unit test page

---
+++ src/views/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="test-container page-container">
+    <div class="page-title">Unit Test Page</div>
+    <p>count is: {{ count }}</p>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'Vuex',
+
+  setup() {
+    const count = ref<number>(0)
+
+    const add = () => {
+      count.value += 1
+    }
+
+    return {
+      count,
+      add
+    }
+  }
+})
+</script>
+
+<style scoped lang="stylus"></style>

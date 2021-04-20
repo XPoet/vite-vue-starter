aa7e1fe1658146c019986f75a1c124eb69e36fc4
XPoet <i@xpoet.cn>
2021-04-20 10:53:44 +0800

    feat(components): add to GitHub link in Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="header">
     <div class="title" @click="router.push('/')">Vite2.x + Vue3.x + TypeScript Starter</div>
+    <div class="go-github" @click="goGitHub"><i class="icon el-icon-s-promotion"></i> GitHub</div>
   </div>
 </template>
 
-<script lang="ts" setup="props">
+<script lang="ts" setup>
 import { useRouter } from 'vue-router'
 
 // eslint-disable-next-line no-unused-vars
 const router = useRouter()
+
+// eslint-disable-next-line no-unused-vars
+const goGitHub = () => {
+  window.open('https://github.com/XPoet/vite-vue3-starter')
+}
 </script>
 
 <style scoped lang="stylus">
@@ -18,15 +24,24 @@ const router = useRouter()
   height 100%
   background #fff
   display flex
-  justify-content flex-start
+  justify-content space-between
   align-items center
-  padding-left 20px
+  padding 0 20px
   box-sizing border-box
+  font-weight bold
 
   .title {
     font-size 20px
-    font-weight bold
     cursor pointer
+  }
+
+  .go-github {
+    cursor pointer
+    font-size 16px
+
+    .icon {
+      font-size 20px
+    }
   }
 }
 </style>

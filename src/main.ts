57cc452546888c2c4c2b02cd912febbab01e61e7
XPoet <i@xpoet.cn>
2021-04-20 23:57:42 +0800

    style: format main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
-/*
- * @Author: Aliom
- * @Date: 2021-04-20 10:03:47
- * @LastEditors: Aliom
- * @LastEditTime: 2021-04-20 10:23:04
- */
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 
 import router from './router/index'
-import { key, store } from './store/index'
+import { key, store } from './store'
 
 import 'element-plus/lib/theme-chalk/index.css'
-
 import './style/basic.styl'
 
 const app = createApp(App)

93287d1a62099b1ffdd00df5c76a36af0936f0d6
XPoet <i@xpoet.cn>
2021-04-22 18:30:58 +0800

    fix(jest): configure the @ point to src in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,8 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest'
   },
   // 匹配 __tests__ 目录下的 .js/.ts 文件 或 xx.test.js/ts xx.spec.js/ts
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$'
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1' // 配置 jest 下 @ -> src
+  }
 }

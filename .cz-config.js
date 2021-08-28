0424ceb591fd5955acc5a9be23c8207233bf22dc
XPoet <i@xpoet.cn>
2021-08-28 14:02:20 +0800

    chore(commitlint): modified commitlint config file

--- .cz-config.js
+++ .cz-config.js
@@ -1,30 +1,50 @@
 module.exports = {
   // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: 'feat', name: 'feat:     新增功能' },
-    { value: 'fix', name: 'fix:      修复 bug' },
-    { value: 'docs', name: 'docs:     文档变更' },
+    {
+      value: 'feat',
+      name: 'feat:     新增功能'
+    },
+    {
+      value: 'fix',
+      name: 'fix:      修复 bug'
+    },
+    {
+      value: 'docs',
+      name: 'docs:     文档变更'
+    },
     {
       value: 'style',
       name: 'style:    代码格式（不影响功能，例如空格、分号等格式修正）'
     },
     {
       value: 'refactor',
-      name: 'refactor: 代码重构（不包括 bug 修复、功能新增）'
+      name: 'refactor:    代码重构（不包括 bug 修复、功能新增）'
     },
-    { value: 'perf', name: 'perf:     性能优化' },
-    { value: 'test', name: 'test:     添加、修改测试用例' },
+    {
+      value: 'perf',
+      name: 'perf:     性能优化'
+    },
+    {
+      value: 'test',
+      name: 'test:     添加、修改测试用例'
+    },
     {
       value: 'build',
-      name:
-        'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
+      name: 'build:    构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）'
     },
-    { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
+    {
+      value: 'ci',
+      name: 'ci:       修改 CI 配置、脚本'
+    },
     {
       value: 'chore',
       name: 'chore:    对构建过程或辅助工具和库的更改（不影响源文件、测试用例）'
     },
-    { value: 'revert', name: 'revert:   回滚 commit' }
+    {
+      value: 'revert',
+      name: 'revert:   回滚 commit'
+    }
   ],
 
   // scope 类型（定义之后，可通过上下键选择）
@@ -32,7 +52,7 @@ module.exports = {
     ['components', '组件相关'],
     ['hooks', 'hook 相关'],
     ['utils', 'utils 相关'],
-    ['element-ui', '对 element-ui 的调整'],
+    ['element-plus', '对 element-plus 的调整'],
     ['styles', '样式相关'],
     ['deps', '项目依赖'],
     ['auth', '对 auth 修改'],

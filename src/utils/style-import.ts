903b7c9a1a6a130b10f2ba17ab052ae63754d95a
XPoet <i@xpoet.cn>
2021-08-31 13:54:30 +0800

    build(deps): vite-plugin-style-import -> vite-plugin-element-plus

--- src/utils/style-import.ts
+++ src/utils/style-import.ts
@@ -3,7 +3,7 @@ import { ElIcon, ElLoading, ElCard, ElButton } from 'element-plus'
 
 /**
  * 按需导入 Element Plus 组件
- * Vite 插件 https://github.com/anncwb/vite-plugin-style-import
+ * Vite 插件 https://github.com/element-plus/vite-plugin-element-plus
  * @param app {App}
  */
 export default function styleImport(app: App) {

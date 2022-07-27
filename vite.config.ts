9005990712f46934606616c0706ed8096e5591e3
XPoet <i@xpoet.cn>
2022-07-27 17:37:58 +0800

    build(vite): 修改 Vite 配置文件，增加开发体验

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 如果编辑器提示 path 模块找不到，安装 @types/node 即可
+// pnpm add @types/node -D
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  base: './', // 打包路径
+  server: {
+    port: 8088, // 服务启动端口号（Vite 3.x 默认 5173）
+    open: true, // 服务启动后自动打开浏览器
+    cors: true // 允许跨域
+  }
 })

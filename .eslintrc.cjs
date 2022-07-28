3e4bd59f0c9a7e4df9807676bdcdfa430a570abf
XPoet <i@xpoet.cn>
2022-07-28 17:57:47 +0800

    build: 集成 ESLint 统一代码风格

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+    'plugin:prettier/recommended' // 添加 prettier 插件，格式化规则：prettier > eslint
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
+};

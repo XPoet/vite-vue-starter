99456c7cb348829342b8411a143cb6dc948469a8
XPoet <i@xpoet.cn>
2022-08-04 15:26:21 +0800

    chore(cz-custom): 更新 cz-custom 配置文件

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,25 +1,31 @@
 module.exports = {
+  // Commit 类型
   types: [
-    { value: 'feat',     name: '✨ feat:      新增功能' },
-    { value: 'fix',      name: '🐛 fix:       修复 Bug' },
-    { value: 'docs',     name: '📝 docs:      文档变更' },
-    { value: 'style',    name: '🎨 style:     不影响代码功能的更改（例如：空格、格式化、补齐分号等）' },
-    { value: 'refactor', name: '🔨 refactor:  代码重构（不包括新增功能、修复 Bug）' },
-    { value: 'perf',     name: '📈 perf:      提高性能的代码更改' },
-    { value: 'test',     name: '✅ test:      添加或修改测试用例' },
-    { value: 'build',    name: '⛏ build:      构建流程、外部依赖变更（如升级依赖、修改 Vite 配置等）' },
-    { value: 'ci',       name: '🔧 ci:        修改 CI 配置、脚本' },
-    { value: 'chore',    name: '📦 chore:     对构建过程或辅助工具和库的更改（不影响源文件、测试用例）' },
-    { value: 'revert',   name: '⏪ revert：   回滚 Commit' },
-    { value: 'WIP',      name: '🚧 WIP:       正在进行的工作' },
-    { value: 'ui',       name: '💄 ui:        修改界面 UI' },
-    { value: 'release',  name: '🚀 release:   发布版本或标签' },
-  ],
+    ['feat', '新增功能', '✨'],
+    ['fix', '修复 Bug', '🐛'],
+    ['docs', '文档变更', '📝'],
+    ['style', '不影响代码功能的更改（例如：空格、格式化、补齐分号等）', '🎨'],
+    ['refactor', '代码重构（不包括新增功能、修复 Bug）', '🔨'],
+    ['perf', '提高性能的代码更改', '📈'],
+    ['test', '新增功能', '✅'],
+    ['build', '构建流程、外部依赖变更（如升级依赖、修改 Vite 配置等）', '🛠️'],
+    ['ci', '修改 CI 配置、脚本', '🔧'],
+    ['chore', '对构建过程或辅助工具和库的更改（不影响源文件、测试用例）', '📦'],
+    ['revert', '回滚 Commit', '⏪'],
+    ['WIP', '正在进行的工作', '🚧'],
+    ['ui', '修改界面 UI', '💄'],
+    ['release', '发布版本或标签', '🚀']
+  ].map(([value, desc, icon]) => {
+    return {
+      value,
+      name: `${(value + ': ').padEnd(10)}${icon} ${desc}`
+    };
+  }),
 
   scopes: [
     ['components', '组件相关'],
     ['hooks', 'hook 相关'],
-    ['utils', 'utils 相关'],
+    ['utils', 'util 相关'],
     ['element-plus', '对 element-plus 的调整'],
     ['styles', '样式相关'],
     ['deps', '项目依赖'],
@@ -29,7 +35,7 @@ module.exports = {
     return {
       value,
       name: `${value.padEnd(20)}（${desc}）`
-    }
+    };
   }),
 
   /*
@@ -46,18 +52,18 @@ module.exports = {
 
   allowCustomScopes: true, // 允许自定义 SCOPE
   allowEmptyScopes: true, // 允许空 SCOPE
-  customScopesName: '自定义 Scope',
-  emptyScopesName: '不填 Scope',
+  customScopesName: `${'custom'.padEnd(20)}（自定义 Scope）`,
+  emptyScopesName: `${'empty'.padEnd(20)}（不填 Scope）`,
 
   messages: {
-    type: "请选择 Commit 类型：（上下键选择，按 Enter 键确定）",
+    type: '请选择 Commit 类型：（上下键选择，按 Enter 键确定）',
     scope: '\n请选择或输入修改范围 Scope：（上下键选择，按 Enter 键确定）',
     customScope: '请输入自定义的 Scope：', // 需设置 allowCustomScopes: true
     subject: '填写简短精炼的变更描述：（必填）\n',
-    body: '填写更加详细的变更描述：（使用 "|" 换行。非必填，可按 Enter 键跳过）\n',
+    body: '填写更加详细的变更描述：（使用 \'|\' 换行。非必填，可按 Enter 键跳过）\n',
     breaking: '列举非兼容性重大的变更：（非必填，可按 Enter 键跳过）\n',
     footer: '列举出所有变更的 ISSUES CLOSED：（例如: #31、#34。非必填，可按 Enter 键跳过）\n',
-    confirmCommit: '确认使用以上信息提交？（y/n/e）',
+    confirmCommit: '确认使用以上信息提交？（y/n/e）'
   },
 
 
@@ -70,8 +76,8 @@ module.exports = {
   // skipQuestions: ['body', 'footer'], // 跳过问题
   skipEmptyScopes: false,
 
-  subjectLimit: 100, // subject 长度限制
+  subjectLimit: 100 // subject 长度限制
   // breaklineChar: '|', // It is supported for fields body and footer.
   // footerPrefix : 'ISSUES CLOSED:'
   // askForBreakingChangeFirst : true, // default is false
-};
+}

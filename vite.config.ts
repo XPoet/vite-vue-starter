a474233d5a990cc4e5e8e56b39c5bb437c79b280
XPoet <i@xpoet.cn>
2024-04-16 22:27:25 +0800

    chore: 配置路径别名 `@` 指向 `/src`

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src' // 设置路径别名：`@` 指向 `src`
+    }
+  },
+  base: './', // 打包路径
+  server: {
+    port: 8889, // 设置端口
+    open: true, // 自动打开
+    cors: true // 允许跨域
+  }
 })
